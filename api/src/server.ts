3e210db390aacab79396ec7dd2191fff9b3ac535
nickbispo <nicolassilvabispo@gmail.com>
2023-09-16 21:03:01 -0300

    criando funcao main, configurando dotenv

--- api/src/server.ts
+++ api/src/server.ts
@@ -4,16 +4,16 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 function main() {
   const app = express();
+  const SERVER_PORT = process.env.SERVER_PORT as string;
 
   const { connectDB } = database();
 
   connectDB();
 
-  app.listen(process.env.SERVER_PORT as string, () => {
-    console.log(`servidor rodando na porta ${3030}`);
+  app.listen(SERVER_PORT, () => {
+    console.log(`servidor rodando na porta ${SERVER_PORT}`);
   });
 }
 
